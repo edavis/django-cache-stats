8203cef95cc656302cd0e5bbe82c70565d0ac911
Eric Davis <ed@npri.org>
2011-06-21 15:49:42 -0700

    Now works with Django 1.3 CACHES setting

--- django_memcached/views.py
+++ django_memcached/views.py
@@ -2,16 +2,42 @@ from django.http import Http404
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.template import RequestContext
-from django.core.cache import parse_backend_uri
+from django.core.exceptions import ImproperlyConfigured
+from django.core.cache import (
+    parse_backend_uri, parse_backend_conf, DEFAULT_CACHE_ALIAS)
 
 from django_memcached.util import get_memcached_stats
 from django.contrib.auth.decorators import user_passes_test
 
-_, hosts, _ = parse_backend_uri(settings.CACHE_BACKEND)
-SERVERS = hosts.split(';')
+def get_cache_server_list():
+    """Returns a list of active memcached servers.
+
+    Compatible with both 1.2 and 1.3 style cache configuration.
+
+    """
+    # If settings.CACHES isn't defined, we're using Django <= 1.2.x
+    if getattr(settings, 'CACHES', None) is None:
+        scheme, hosts, params  = parse_backend_uri(settings.CACHE_BACKEND)
+        if not 'memcached' in scheme:
+            raise ImproperlyConfigured("Must use memcached.  Currently using '%s'" % scheme)
+
+    # Django >= 1.3
+    #
+    # Django silently converts old-style CACHE_* arguments into the new-style
+    # CACHES dictionary.  So we're safe to use this.
+    else:
+        engine, hosts, params = parse_backend_conf(DEFAULT_CACHE_ALIAS)
+        if 'memcached' not in engine:
+            raise ImproperlyConfigured("Must use memcached.  Currently using '%s'" % engine)
+
+    return hosts if isinstance(hosts, list) else hosts.split(';')
 
 def server_list(request):
-    statuses = zip(range(len(SERVERS)), SERVERS, map(get_memcached_stats, SERVERS))
+    servers = get_cache_server_list()
+    statuses = []
+    for idx, server in enumerate(servers):
+        statuses.append((idx, server, get_memcached_stats(server)))
+
     context = {
         'statuses': statuses,
     }
@@ -22,16 +48,13 @@ def server_list(request):
     )
 
 def server_status(request, index):
+    servers = get_cache_server_list()
     try:
         index = int(index)
     except ValueError:
         raise Http404
-    if 'memcached' not in settings.CACHE_BACKEND:
-        raise Http404
-    if not SERVERS:
-        raise Http404
     try:
-        server = SERVERS[index]
+        server = servers[index]
     except IndexError:
         raise Http404
     stats = get_memcached_stats(server)
