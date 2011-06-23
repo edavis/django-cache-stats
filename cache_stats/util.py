4840e886e642cbe86b64c61c37927a318f4cfea0
Eric Davis <circa1979@gmail.com>
2011-06-22 20:08:55 -0700

    Clean up get_stats

--- cache_stats/util.py
+++ cache_stats/util.py
@@ -1,32 +1,39 @@
 import datetime
+from decimal import Decimal
 from django.core.cache import cache
 
 def get_stats():
-    stats = cache._cache.get_stats()
-    new_stats = {}
+    c = cache._cache
+    stats = dict(c.get_stats())
+    servers = stats.keys()
 
-    for server, server_stats in stats:
-        info = server_stats.copy()
-        new_stats[server] = info
-        for memcached_key, memcached_value in server_stats.iteritems():
-            if memcached_key == "uptime":
-                info["uptime"] = datetime.timedelta(seconds=int(memcached_value))
-            elif memcached_key == "time":
-                info["time"] = datetime.datetime.fromtimestamp(int(memcached_value))
-            else:
-                try:
-                    info[memcached_key] = int(memcached_value)
-                except ValueError:
-                    pass
+    conversion_map = {
+        "uptime": lambda n: datetime.timedelta(seconds=int(n)),
+        "rusage_system": Decimal,
+        "rusage_user": Decimal,
+        "time": lambda n: datetime.datetime.fromtimestamp(int(n)),
+    }
 
-        try:
-            info["hit_rate"] = 100. * info["get_hits"] / float(info["cmd_get"])
-        except ZeroDivisionError:
-            info["hit_rate"] = info["get_hits"]
+    cache_hosts = {}
 
-        info["started_at"] = info["time"] - info["uptime"]
+    for server in servers:
+        cache_hosts[server] = {}
+        info = stats[server].copy()
 
-        info["cmd_get_per_second"] = info["cmd_get"] / float(info["uptime"].seconds)
-        info["cmd_set_per_second"] = info["cmd_set"] / float(info["uptime"].seconds)
+        for k, v in info.items():
+            func = conversion_map.get(k)
 
-    return new_stats
+            if func is not None:
+                info[k] = func(v)
+
+            elif v.isdigit():
+                info[k] = int(v)
+
+        info["hit_rate"] = 100.0 * info["get_hits"] / float(info["cmd_get"])
+        info["gets_per_second"] = info["cmd_get"] / float(info["uptime"].seconds)
+        info["sets_per_second"] = info["cmd_set"] / float(info["uptime"].seconds)
+        info["started_at"] = datetime.datetime.now() - info["uptime"]
+
+        cache_hosts[server].update(info)
+
+    return cache_hosts
